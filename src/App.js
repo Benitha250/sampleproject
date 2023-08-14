7c435bfe01fcd39e1d859e19e314d022b36eccac
Benitha250 <benithauwase05@gmail.com>
2023-08-14 16:40:27 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
 
     return (
         <AnimatePresence>
-        <div className="w-screen h-auto flex flex-col">
+            <div className="w-screen h-auto flex flex-col">
                 <Header />
 
                 <main className="mt-24 p-8 w-full">
